90f8872ad1919fe7418f3112e36cbf2ac5d898c3
aaron <asdf>
2023-05-14 23:30:57 +0200

    no flask

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app:telegram_api
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from telegram_api import main as start_telegram
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    start_telegram()
+    # start_telegram()
     return "<p>Telegram started!</p>"
