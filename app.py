35758a5b4534fd058a9c16d74bf16e1d96b6da18
aaron <asdf>
2023-05-14 22:38:35 +0200

    expanded flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from telegram_api import main as start_telegram
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    start_telegram()
+    return "<p>Telegram started!</p>"
